961aa50eb00cf11f3ecc092ffd92aeb531af035f
zhaoly899 <zhaoly@aixiongan.org.cn>
2026-06-01 16:32:16 +0800

    Initial commit: aixiongan-docs project

---
+++ admin/adduser_dir.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+while IFS=, read USER EMAIL UIDD GIDD
+do
+  username=$(echo "$USER" | tr -d '\r')
+  if [ -n "$username" ]; then
+    echo "username : $username"	
+    
+    yamlfile=`cat ./values-template.yaml`
+    all_variables="NAMESPACE=$username EMAIL=$(echo "$EMAIL" | tr -d '\r')"
+    
+    if [ ! -d "./yamls/" ];then
+        mkdir ./yamls
+    fi
+    printf "$all_variables\ncat << EOF\n$yamlfile\nEOF" | bash > ./yamls/values_$username.yaml
+
+    kubectl create namespace $username
+
+    helm install admin-$username \
+      --namespace=admin-helm \
+      --create-namespace \
+      --values ./yamls/values_$username.yaml \
+      ./adminchart
+
+    # helm install gfshome-$username \
+    #   --namespace=admin-helm \
+    #   --create-namespace \
+    #   --values ./yamls/values_$username.yaml \
+    #   ./gfshomechart
+
+    # helm install gfsshare-$username \
+    #   --namespace=admin-helm \
+    #   --create-namespace \
+    #   --values ./yamls/values_$username.yaml \
+    #   ./gfssharechart
+    
+    # helm install ssdshare-$username \
+    #   --namespace=admin-helm \
+    #   --create-namespace \
+    #   --values ./yamls/values_$username.yaml \
+    #   ./ssdsharechart
+    
+     testuser=$(helm install testuser-$username \
+       --namespace=$username \
+       --values ./yamls/values_$username.yaml \
+       ../user/userchart)
+
+    # if [[ ($testuser =~ $username)  && ($testuser =~ "deployed")]]
+    # then
+    #     echo "成功 创建用户 $username"
+    # else
+    #     echo "失败 创建用户 $username"
+    # fi
+
+  fi
+done < $1
---
+++ admin/deluser_dir.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+while IFS=, read USER EMAIL UIDD GIDD
+do
+  if [ -n "$(echo "$USER" | tr -d '\r')" ]; then
+    echo "username : $USER"	
+
+    if [ ! -d "./yamls/values_$(echo "$USER" | tr -d '\r').yaml" ];then
+      echo "delete delete pods pvcs namespace in k8s"
+      helm delete admin-$(echo "$USER" | tr -d '\r') --namespace=admin-helm
+      # helm delete gfshome-$(echo "$USER" | tr -d '\r') --namespace=admin-helm
+      # helm delete gfsshare-$(echo "$USER" | tr -d '\r') --namespace=admin-helm
+      # helm delete ssdshare-$(echo "$USER" | tr -d '\r') --namespace=admin-helm
+      helm delete testuser-$(echo "$USER" | tr -d '\r')  --namespace=$(echo "$USER" | tr -d '\r')
+      kubectl delete namespace $(echo "$USER" | tr -d '\r')
+    else
+      echo "there is no yaml file for $USER"
+    fi
+  fi
+done < $1
